9a111140d8521e0f5df916c09a46184898b59211
Oishi1029 <149283931+Oishi1029@users.noreply.github.com>
2024-11-06 21:34:30 +0800

    Update gateDriver3.m

--- conditioningGDP/gateDriver3.m
+++ conditioningGDP/gateDriver3.m
@@ -1,11 +1,19 @@
-area_copperWire = [0,0,0,0,0]
+area_copperWire = [0,0,0,0,0];
+diameter_windingWindow_MIN_inMM = 6.8 - 0.35;
+diameter_windingWindow_MIN_inM = diameter_windingWindow_MIN_inMM*10^-3;
+A_windingWindow = pi*(diameter_windingWindow_MIN_inM/2)^2
+
 A_core = 1.22e-5; %TN13
 numOfTurn = 17;
 
 %initialize array
 fillFactor = [0,0,0,0,0];
+diameter_copperWire = [0.884e-3,0.789e-3,0.63e-3,0.367e-3,0.312e-3];
+conditionA = [0,0,0,0,0];
+conditionB = [0,0,0,0,0];
 
 for i = 1:5
     area_copperWire(i) = pi*(diameter_copperWire(i)/2)^2
-    fillFactor(i) = (3*numOfTurn*area_copperWire(i))/A_core
+    fillFactor(i) = (3*numOfTurn*area_copperWire(i))/A_windingWindow
+    
 end
