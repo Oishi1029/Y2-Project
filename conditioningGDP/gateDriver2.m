01c0c75a2d666f6a602fb017e0a1aae71c0cc3fe
Oishi1029 <149283931+Oishi1029@users.noreply.github.com>
2024-11-06 20:54:21 +0800

    Update gateDriver2.m

--- conditioningGDP/gateDriver2.m
+++ conditioningGDP/gateDriver2.m
@@ -4,9 +4,10 @@ f_operating = 100e3;
 
 %to calculate skin depth
 freeSpaceP = 4*pi*10^-7; 
-relativeP_copperWire = 1; % 3C90
+relativeP_copperWire = 0.999994; % 3C90
 resistivity_copperWire = 1.68e-8;
 skinDepth = sqrt(resistivity_copperWire/(pi*f_operating*relativeP_copperWire*freeSpaceP))
+
 
 L_magGateDriver = 349e-6;
 Vs_gateDriver = 15;
@@ -16,7 +17,7 @@ VTA = (Vs_gateDriver*d_gateDriver*T_gateDriver);
 currentMagnetisingGateDriver = VTA/L_magGateDriver
 
 
-diameter_copperWire = [0.8e-3,0.7e-3,0.56e-3,0.315e-3,0.25e-3];
+diameter_copperWire = [0.884e-3,0.789e-3,0.63e-3,0.367e-3,0.312e-3];
 conversion_factor = 1e6;  % 1,000,000 (10^6)
 
 %initialize arrays
@@ -24,11 +25,19 @@ area_copperWire = [0,0,0,0,0]
 area_skindepth = [0,0,0,0,0];
 area_empty = [0,0,0,0,0];
 radius_empty = [0,0,0,0,0];
+radius_copperWire = [0,0,0,0,0];
+debug = [0,0,0,0,0];
 currentDensity_copperWire = [0,0,0,0,0];
 currentDensity_copperWire_inMM = [0,0,0,0,0];
 
 for i = 1:5
+    display(i)
+    
+    radius_copperWire(i) = diameter_copperWire(i)/2
+
     radius_empty(i) = (diameter_copperWire(i)-2*skinDepth)/2
+
+    debug(i) = radius_copperWire(i)-skinDepth
 
     area_copperWire(i) = pi*(diameter_copperWire(i)/2)^2
 
@@ -40,6 +49,11 @@ for i = 1:5
 
     currentDensity_copperWire_inMM(i) = currentDensity_copperWire(i) / conversion_factor
 end
+
+
+
+
+
 
 
 
