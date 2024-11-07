be39021bf5bfc700148c8e4958d7a5c1b8ba49b3
Oishi1029 <149283931+Oishi1029@users.noreply.github.com>
2024-11-07 14:41:45 +0800

    Update gateDriver3_fillFactor.m

--- conditioningGDP/gateDriver3_fillFactor.m
+++ conditioningGDP/gateDriver3_fillFactor.m
@@ -1,19 +1,68 @@
+%format long
+format short
+
+%initialize array
 area_copperWire = [0,0,0,0,0];
-diameter_windingWindow_MIN_inMM = 6.8 - 0.35;
+N_min = [0,0,0,0,0];
+N_max = [0,0,0,0,0];
+Kfill_calculated = [0,0,0,0,0]
+result_calculated = [0,0,0,0,0]
+result_required = [0,0,0,0,0]
+
+%adjust the tolerance accordingly, can explain in report for different
+%Kfill value
+diameter_windingWindow_MIN_inMM = 6.8-0.35;  
+
+
 diameter_windingWindow_MIN_inM = diameter_windingWindow_MIN_inMM*10^-3;
 A_windingWindow = pi*(diameter_windingWindow_MIN_inM/2)^2
 
 A_core = 1.22e-5; %TN13
-numOfTurn = 17;
+numOfTurn_calculated = 18;
 
-%initialize array
-fillFactor = [0,0,0,0,0];
+%adjust the tolerance accordingly, can explain in report for different
+%Kfill value
 diameter_copperWire = [0.884e-3,0.789e-3,0.63e-3,0.367e-3,0.312e-3];
-conditionA = [0,0,0,0,0];
-conditionB = [0,0,0,0,0];
+diameter_copperWire_withMaxTolerance = [0.892e-3,0.796e-3,0.636e-3,0.371e-3,0.316e-3]; 
+
+Kfill_min = 0.2;
+Kfill_max = 0.4;
+
+Kfill_required = [0,0,0,0,0.3]
+numOfTurn_required = [0,0,0,0,0]
+
 
 for i = 1:5
-    area_copperWire(i) = pi*(diameter_copperWire(i)/2)^2
-    fillFactor(i) = (3*numOfTurn*area_copperWire(i))/A_windingWindow
+    display(i)
+    area_copperWire(i) = pi*(diameter_copperWire_withMaxTolerance(i)/2)^2
+    Kfill_calculated(i) =  (3*numOfTurn_calculated*area_copperWire(i))/A_windingWindow
+    
+    %condition_left_calculated = Kfill_calculated(i)*A_windingWindow
+    %condition_right_calculated = 3*numOfTurn_calculated*area_copperWire(i)
+    %numOfTurn_required(i) = (Kfill_required(i) * A_windingWindow)/ (3*area_copperWire(i))
+
+    condition_left_required = Kfill_required(i)*A_windingWindow
+    condition_right_required = 3*numOfTurn_calculated*area_copperWire(i)
+    
+    
+    
+    %{
+    if condition_left_calculated > condition_right_calculated
+    result_calculated(i) = 1
+    else 
+    result_calculated(i) = 0
+    end
+    %}
+
+    if condition_left_required > condition_right_required
+    result_required(i) = 1
+    else 
+    result_required(i) = 0
+    end
+    
+        
+
+    %N_min(i) = (Kfill_min*A_windingWindow)/(3*area_copperWire(i))
+    %N_max(i) = (Kfill_max*A_windingWindow)/(3*area_copperWire(i))
     
 end
