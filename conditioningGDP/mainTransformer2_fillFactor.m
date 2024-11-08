cf0baea1e31ed304173e6960f8a716f85a6e12b7
Oishi1029 <149283931+Oishi1029@users.noreply.github.com>
2024-11-08 17:12:01 +0800

    Update mainTransformer2_fillFactor.m

--- conditioningGDP/mainTransformer2_fillFactor.m
+++ conditioningGDP/mainTransformer2_fillFactor.m
@@ -9,11 +9,6 @@ Kfill_calculated = [0,0,0,0,0]
 result_calculated = [0,0,0,0,0]
 result_required = [0,0,0,0,0];
 
-%to calculate area of winding window
-%height = (2*11.8)*10^-3;
-%width = ((25.6-11.1)/2)*10^-3;
-%A_windingWindow = 2*(height*width)
-%numOfTurn_calculated = 12;
 
 A_windingWindow_inMM = 122;
 A_windingWindow_inM = 122*10^-6;
@@ -22,9 +17,6 @@ A_windingWindow_inM = 122*10^-6;
 %Kfill value
 diameter_copperWire = [0.884e-3,0.789e-3,0.63e-3,0.367e-3,0.312e-3];
 diameter_copperWire_withMaxTolerance = [0.892e-3,0.796e-3,0.636e-3,0.371e-3,0.316e-3]; 
-
-Kfill_min = 0.2;
-Kfill_max = 0.4;
 
 Kfill_required = [0,0,0,0,0.0]
 numOfTurn_required = [0,0,0,0,0]
